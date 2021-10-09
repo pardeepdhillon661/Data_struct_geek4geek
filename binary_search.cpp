9d0cbf78a3b622c16a246bb15d0d9c2d7c4e5cf7
pardeepdhillon661 <92221868+pardeepdhillon661@users.noreply.github.com>
2021-10-09 21:28:46 +0530

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rec(int arr[], int n, int &k) {
+    sum = max()
+}
+
+int solve(int arr[], int n) {
+    int k = 1;
+    int ans = rec(arr, n, k);
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    cout << solve(arr, n);
+    return 0;
+}
